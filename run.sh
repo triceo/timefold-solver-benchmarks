e02e6bdd8195de17d18c2c66e369540de1b866bf
Lukáš Petrovický <lukas@petrovicky.net>
2023-07-11 12:36:20 +0200

    Run on two non-HT cores

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo -i sysctl kernel.perf_event_paranoid=1
 sudo -i sysctl kernel.kptr_restrict=0
-nohup java -jar target/benchmarks.jar > target/nohup.out 2>&1 &
+nohup taskset -c 0,2 java -jar target/benchmarks.jar > target/nohup.out 2>&1 &
